22877cd3db20a60e724c356dc7d19ab8ee57e6ef
DJ Knox <dhknox@gmail.com>
2019-06-13 18:30:41 -0400

    add function for calculating RGBA alpha values of elevation data

--- pathfinder.py
+++ pathfinder.py
@@ -1,3 +1,5 @@
+from PIL import Image
+
 def read_elevation_data_into_2d_list(file):
     """
     Given a file, read each row in the data and split into a list of integers.
@@ -12,9 +14,70 @@ def read_elevation_data_into_2d_list(file):
     return elevation_data
 
 
+def find_max_elevation(elevation_data):
+    """
+    Given a two-dimensional array, return the highest value in the array.
+    """
+    max_value = max(elevation_data[0])
+    for row in elevation_data:
+        for column in row:
+            if column > max_value:
+                max_value = column
+    return max_value
+    
+
+def find_min_elevation(elevation_data):
+    """
+    Given a two-dimensional array, return the lowest value in the array.
+    """
+    min_value = min(elevation_data[0])
+    for row in elevation_data:
+        for column in row:
+            if column < min_value:
+                min_value = column
+    return min_value
+
+
+def calculate_alpha_value(min_elevation, max_elevation, elevation_value):
+    """
+    Given a minimum elevation value, a maximum elevation value, and a single elevation value, calculate and return the alpha value (opacity setting in RGBA).
+    """
+    return int(((elevation_value - min_elevation) / (max_elevation - min_elevation)) * 255)
+
+
 if __name__ == "__main__":
-    # open elevation_small.txt and read into 2d list
+    # Read the data from elevation_small.txt into an appropriate data structure to get the elevation data.
     with open('elevation_small.txt') as file:
         elevation_data = read_elevation_data_into_2d_list(file)
 
-    print(elevation_data[0])
+    # Using the Pillow library, create an elevation map from the data. Higher elevations should be brighter; lower elevations darker.
+    image_width = len(elevation_data[0])
+    image_height = len(elevation_data)
+    min_elevation = find_min_elevation(elevation_data)
+    max_elevation = find_max_elevation(elevation_data)
+
+    # greyscale
+    # image = Image.new('L', (image_width, image_height))
+    # for row in elevation_data:
+    #     for column in row:
+    #         row_index = elevation_data.index(row)
+    #         column_index = elevation_data[row_index].index(column)
+    #         alpha_value = calculate_alpha_value(min_elevation, max_elevation, column)
+    #         image.putpixel((row_index, column_index), alpha_value)
+    # image.save('map.png')
+
+    image = Image.new('RGBA', (image_width, image_height), (0, 0, 0, 0))
+    for row in elevation_data:
+        for column in row:
+            row_index = elevation_data.index(row)
+            column_index = elevation_data[row_index].index(column)
+            alpha_value = calculate_alpha_value(min_elevation, max_elevation, column)
+            image.putpixel((row_index, column_index), (255, 255, 255, alpha_value))
+    image.save('map.png')
+
+    # TODO:
+    # figure out the putpixel() part of the above loop(s) - specifically the second parameter (column)
+
+
+
+
--- test_pathfinder.py
+++ test_pathfinder.py
@@ -1,8 +1,23 @@
 import pathfinder
 
-file = open('elevation_small.txt')
-
 def test_file_can_be_read_into_a_2d_list():
+    # the first value in elevation_small is 4713
+    # the last value in elevation_small is 3948
+    file = open('elevation_small.txt')
     elevation_data = pathfinder.read_elevation_data_into_2d_list(file)
     assert(elevation_data[0][0] == 4713)
     assert(elevation_data[-1][-1] == 3948)
+
+def test_max_elevation_can_be_found():
+    data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    assert(pathfinder.find_max_elevation(data) == 9)
+
+def test_min_elevation_can_be_found():
+    data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    assert(pathfinder.find_min_elevation(data) == 1)
+
+def test_alpha_value_can_be_calculated():
+    min_value = 1
+    max_value = 9
+    assert(pathfinder.calculate_alpha_value(min_value, max_value, 9) == 255)
+    assert(pathfinder.calculate_alpha_value(min_value, max_value, 1) == 0)
