06dc7b2e6e919e5ddc4442ecb8a21d5395f9635e
DJ Knox <dhknox@gmail.com>
2019-06-14 12:23:47 -0400

    add functions to draw an elevation map in RGBA and greyscale

--- pathfinder.py
+++ pathfinder.py
@@ -45,26 +45,39 @@ def calculate_alpha_value(min_elevation, max_elevation, elevation_value):
     return int(((elevation_value - min_elevation) / (max_elevation - min_elevation)) * 255)
 
 
-if __name__ == "__main__":
-    # Read the data from elevation_small.txt into an appropriate data structure to get the elevation data.
-    with open('elevation_small.txt') as file:
-        elevation_data = read_elevation_data_into_2d_list(file)
+def draw_elevation_map_grey_scale(elevation_data):
+    """
+    Given a 2d array of elevation data, draw an elevation map in greyscale and save as 'map.png'.        
+    """
+    print("Drawing an elevation map in greyscale...")
 
-    # Using the Pillow library, create an elevation map from the data. Higher elevations should be brighter; lower elevations darker.
     image_width = len(elevation_data[0])
     image_height = len(elevation_data)
     min_elevation = find_min_elevation(elevation_data)
     max_elevation = find_max_elevation(elevation_data)
 
-    # greyscale
-    # image = Image.new('L', (image_width, image_height))
-    # for row in elevation_data:
-    #     for column in row:
-    #         row_index = elevation_data.index(row)
-    #         column_index = elevation_data[row_index].index(column)
-    #         alpha_value = calculate_alpha_value(min_elevation, max_elevation, column)
-    #         image.putpixel((column_index, row_index), alpha_value)
-    # image.save('map.png')
+    image = Image.new('L', (image_width, image_height))
+    for row in elevation_data:
+        for column in row:
+            row_index = elevation_data.index(row)
+            column_index = elevation_data[row_index].index(column)
+            alpha_value = calculate_alpha_value(min_elevation, max_elevation, column)
+            image.putpixel((column_index, row_index), alpha_value)
+    image.save('map.png')
+
+    print("Done drawing!")
+
+
+def draw_elevation_map_rgba(elevation_data):
+    """
+    Given a 2d array of elevation data, draw an elevation map in RGBA and save as 'map.png'.
+    """
+    print("Drawing elevation map in RGBA...")
+
+    image_width = len(elevation_data[0])
+    image_height = len(elevation_data)
+    min_elevation = find_min_elevation(elevation_data)
+    max_elevation = find_max_elevation(elevation_data)
 
     image = Image.new('RGBA', (image_width, image_height), (0, 0, 0, 0))
     for row in elevation_data:
@@ -75,9 +88,17 @@ if __name__ == "__main__":
             image.putpixel((column_index, row_index), (255, 255, 255, alpha_value))
     image.save('map.png')
 
+    print("Done drawing!")
+
+
+if __name__ == "__main__":
+    # Read the data from elevation_small.txt into an appropriate data structure to get the elevation data.
+    with open('elevation_small.txt') as file:
+        elevation_data = read_elevation_data_into_2d_list(file)
+
+    # Using the Pillow library, create an elevation map from the data. Higher elevations should be brighter; lower elevations darker.
+    draw_elevation_map_rgba(elevation_data)
+
     # TODO:
-    # make map.png look like example in README
-
-
-
-
+    # make map.png look like exactly like example in README (?)
+    # add the ability to start from the left edge of the map on any row (y-position) and calculate and draw a path across the map, using the greedy algorithm
