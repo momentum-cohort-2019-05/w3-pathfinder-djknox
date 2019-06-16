54ca1fbdba756fd271f2f2dda5771b4666ab2abe
DJ Knox <dhknox@gmail.com>
2019-06-16 13:54:54 -0400

    organize pathfinder module into classes

--- pathfinder.py
+++ pathfinder.py
@@ -3,169 +3,190 @@ from PIL import Image
 # import random module for flipping a coin in the case of ties in the greedy algorithm
 import random
 
-def read_elevation_data_into_2d_list(file):
-    """
-    Given a file, read each row in the data and split into a list of integers.
-    Append each list to another list to create a 2d list to return.
-    """
-    elevation_data = []
-    for row in file:
-        elevations = row.split()
-        elevations = [int(elevation_as_string) for elevation_as_string in elevations]
-        elevation_data.append(elevations)
 
-    return elevation_data
+class ElevationMap:
+    """
+    ElevationMap is a class for representing elevation data from a text file.
+    """
+    def __init__(self, elevation_data_filename):
+        self.elevation_data = self.read_elevation_data_into_2d_list(elevation_data_filename)
 
 
-def find_max_elevation(elevation_data):
+    def read_elevation_data_into_2d_list(self, filename):
+        """
+        Given a file, read each row in the data and split into a list of integers.
+        Append each list to another list to create a 2d list to return.
+        """
+        with open(filename) as file:
+            elevation_data = []
+            for row in file:
+                elevations = row.split()
+                elevations = [int(elevation_as_string) for elevation_as_string in elevations]
+                elevation_data.append(elevations)
+
+        return elevation_data
+
+
+class ElevationMapPainter:
     """
-    Given a two-dimensional array, return the highest value in the array.
+    ElevationMapPainter is a class for taking an ElevationMap object and drawing an elevation map based on its data.
     """
-    max_value = max(elevation_data[0])
-    for row in elevation_data:
-        for column in row:
-            if column > max_value:
-                max_value = column
-    return max_value
+    def __init__(self, elevation_map):
+        self.elevation_data = elevation_map.elevation_data
+        self.min_elevation = self.find_min_elevation()
+        self.max_elevation = self.find_max_elevation()
+        self.image_width = len(self.elevation_data[0])
+        self.image_height = len(self.elevation_data)
+
+
+    def find_max_elevation(self):
+        """
+        Given a two-dimensional array, return the highest value in the array.
+        """
+        max_value = max(self.elevation_data[0])
+        for row in self.elevation_data:
+            for column in row:
+                if column > max_value:
+                    max_value = column
+        return max_value
     
 
-def find_min_elevation(elevation_data):
-    """
-    Given a two-dimensional array, return the lowest value in the array.
-    """
-    min_value = min(elevation_data[0])
-    for row in elevation_data:
-        for column in row:
-            if column < min_value:
-                min_value = column
-    return min_value
+    def find_min_elevation(self):
+        """
+        Given a two-dimensional array, return the lowest value in the array.
+        """
+        min_value = min(self.elevation_data[0])
+        for row in self.elevation_data:
+            for column in row:
+                if column < min_value:
+                    min_value = column
+        return min_value
 
 
-def calculate_alpha_value(min_elevation, max_elevation, elevation_value):
-    """
-    Given a minimum elevation value, a maximum elevation value, and a single elevation value, calculate and return the alpha value (opacity setting in RGBA).
-    """
-    return int(((elevation_value - min_elevation) / (max_elevation - min_elevation)) * 255)
+    def calculate_alpha_value(self, elevation_value):
+        """
+        Given an elevation value, calculate and return the alpha value (opacity setting in RGBA).
+        """
+        return int(((elevation_value - self.min_elevation) / (self.max_elevation - self.min_elevation)) * 255)
 
 
-def draw_elevation_map_grey_scale(elevation_data):
-    """
-    Given a 2d list of elevation data, draw an elevation map in greyscale and save as 'map.png'.        
-    """
-    print("Drawing an elevation map in greyscale...")
+    def draw_elevation_map_grey_scale(self):
+        """
+        Given a 2d list of elevation data, draw an elevation map in greyscale and save as 'map.png'.        
+        """
+        print("Drawing an elevation map in greyscale...")
 
-    image_width = len(elevation_data[0])
-    image_height = len(elevation_data)
-    min_elevation = find_min_elevation(elevation_data)
-    max_elevation = find_max_elevation(elevation_data)
+        image = Image.new('L', (self.image_width, self.image_height))
+        for row in self.elevation_data:
+            for column in row:
+                row_index = self.elevation_data.index(row)
+                column_index = self.elevation_data[row_index].index(column)
+                alpha_value = self.calculate_alpha_value(column)
+                image.putpixel((column_index, row_index), alpha_value)
+        image.save('map.png')
 
-    image = Image.new('L', (image_width, image_height))
-    for row in elevation_data:
-        for column in row:
-            row_index = elevation_data.index(row)
-            column_index = elevation_data[row_index].index(column)
-            alpha_value = calculate_alpha_value(min_elevation, max_elevation, column)
-            image.putpixel((column_index, row_index), alpha_value)
-    image.save('map.png')
-
-    print("Done drawing elevation map!")
-    return image
+        print("Done drawing elevation map!")
+        return image
 
 
-def draw_elevation_map_rgba(elevation_data):
-    """
-    Given a 2d list of elevation data, draw an elevation map in RGBA and save as 'map.png'.
-    """
-    print("Drawing elevation map in RGBA...")
+    def draw_elevation_map_rgba(self):
+        """
+        Given a 2d list of elevation data, draw an elevation map in RGBA and save as 'map.png'.
+        """
+        print("Drawing elevation map in RGBA...")
 
-    image_width = len(elevation_data[0])
-    image_height = len(elevation_data)
-    min_elevation = find_min_elevation(elevation_data)
-    max_elevation = find_max_elevation(elevation_data)
+        image = Image.new('RGBA', (self.image_width, self.image_height), (0, 0, 0, 0))
+        for row in self.elevation_data:
+            for column in row:
+                row_index = self.elevation_data.index(row)
+                column_index = self.elevation_data[row_index].index(column)
+                alpha_value = self.calculate_alpha_value(column)
+                image.putpixel((column_index, row_index), (255, 255, 255, alpha_value))
+        image.save('map.png')
 
-    image = Image.new('RGBA', (image_width, image_height), (0, 0, 0, 0))
-    for row in elevation_data:
-        for column in row:
-            row_index = elevation_data.index(row)
-            column_index = elevation_data[row_index].index(column)
-            alpha_value = calculate_alpha_value(min_elevation, max_elevation, column)
-            image.putpixel((column_index, row_index), (255, 255, 255, alpha_value))
-    image.save('map.png')
-
-    print("Done drawing elevation map!")
-    return image
+        print("Done drawing elevation map!")
+        return image
 
 
-def greedy_walk_to_next_pixel(elevation_data, start_row, start_column):
-    """
-    Given a 2d list, a start row, and a start column, return the next row and next column in the list for the element that has the least difference.
-    """
-    current_pixel = elevation_data[start_row][start_column]
-    next_pixel_column = start_column + 1
-    forward_pixel = elevation_data[start_row][next_pixel_column]
-    forward_top_pixel = elevation_data[start_row - 1][next_pixel_column]
-    forward_bottom_pixel = elevation_data[start_row + 1][next_pixel_column]
-    difference_in_forward = abs(current_pixel - forward_pixel)
-    difference_in_forward_top = abs(current_pixel - forward_top_pixel)
-    difference_in_forward_bottom = abs(current_pixel - forward_bottom_pixel)
+    def draw_path_of_least_resistance(self, image, pixel_row, pixel_column):
+        """
+        Given an image and a starting pixel row and column, draw a path on the image using the greedy algorithm.
+        """
+        print(f"Drawing path of least resistance on map, starting at pixel coordinate: ({pixel_row}, {pixel_column})...")
+        
+        greedy_algorithm = GreedyAlgorithm(self.elevation_data)
+        for pixel in range(self.image_width - 1):
+            pixel_row, pixel_column = greedy_algorithm.greedy_walk_to_next_pixel(pixel_row, pixel_column)
+            image.putpixel((pixel_column, pixel_row), (0, 0, 255, 255))
+        image.save('map_path.png')
 
-    # if difference_in_forward has the least difference
-    if difference_in_forward_top > difference_in_forward < difference_in_forward_bottom:
-        next_pixel_row = start_row
+        print("Done drawing path of least resistance on map!")
+        return image
 
-    # if difference_in_forward is tied with any other pixels
-    if difference_in_forward == difference_in_forward_top or difference_in_forward == difference_in_forward_bottom:
-        next_pixel_row = start_row
-    
-    # if difference_in_forward_top has the least difference
-    if difference_in_forward > difference_in_forward_top < difference_in_forward_bottom:
-        next_pixel_row = start_row - 1
 
-    # if difference_in_forward_bottom has the least difference
-    if difference_in_forward_top > difference_in_forward_bottom < difference_in_forward:
-        next_pixel_row = start_row + 1
+class GreedyAlgorithm:
+    def __init__(self, elevation_data):
+        self.elevation_data = elevation_data
 
-    # if difference_in_forward_top is tied with difference_in_forward_bottom
-    if difference_in_forward_top == difference_in_forward_bottom:
-        list_of_diffs = [forward_top_pixel, forward_bottom_pixel]
-        coin_flip_result = random.randint(0, 1)
-        chosen_pixel = list_of_diffs[coin_flip_result]
+    def greedy_walk_to_next_pixel(self, start_row, start_column):
+        """
+        Given a start row and a start column, return the next row and next column in the list for the element that has the least difference in elevation.
+        """
+        current_pixel = self.elevation_data[start_row][start_column]
+        next_pixel_column = start_column + 1
+        forward_pixel = self.elevation_data[start_row][next_pixel_column]
+        forward_top_pixel = self.elevation_data[start_row - 1][next_pixel_column]
+        forward_bottom_pixel = self.elevation_data[start_row + 1][next_pixel_column]
+        difference_in_forward = abs(current_pixel - forward_pixel)
+        difference_in_forward_top = abs(current_pixel - forward_top_pixel)
+        difference_in_forward_bottom = abs(current_pixel - forward_bottom_pixel)
 
-        if chosen_pixel == forward_top_pixel:
+        # if difference_in_forward has the least difference
+        if difference_in_forward_top > difference_in_forward < difference_in_forward_bottom:
+            next_pixel_row = start_row
+
+        # if difference_in_forward is tied with any other pixels
+        if difference_in_forward == difference_in_forward_top or difference_in_forward == difference_in_forward_bottom:
+            next_pixel_row = start_row
+        
+        # if difference_in_forward_top has the least difference
+        if difference_in_forward > difference_in_forward_top < difference_in_forward_bottom:
             next_pixel_row = start_row - 1
 
-        elif chosen_pixel == forward_bottom_pixel:
+        # if difference_in_forward_bottom has the least difference
+        if difference_in_forward_top > difference_in_forward_bottom < difference_in_forward:
             next_pixel_row = start_row + 1
 
-    return next_pixel_row, next_pixel_column
+        # if difference_in_forward_top is tied with difference_in_forward_bottom
+        if difference_in_forward_top == difference_in_forward_bottom:
+            list_of_diffs = [forward_top_pixel, forward_bottom_pixel]
+            coin_flip_result = random.randint(0, 1)
+            chosen_pixel = list_of_diffs[coin_flip_result]
 
+            if chosen_pixel == forward_top_pixel:
+                next_pixel_row = start_row - 1
 
-def draw_path_of_least_resistance(image, elevation_data, pixel_row, pixel_column):
-    """
-    Given an image, a 2d list of elevation data, and a starting pixel row and column, draw the path of least resistance on the image using the greedy algorithm.
-    """
-    print("Drawing path of least resistance on map...")
-    for pixel in range(len(elevation_data[0]) - 1):
-        pixel_row, pixel_column = greedy_walk_to_next_pixel(elevation_data, pixel_row, pixel_column)
-        image.putpixel((pixel_column, pixel_row), (0, 0, 255, 255))
-    image.save('map_path.png')
+            elif chosen_pixel == forward_bottom_pixel:
+                next_pixel_row = start_row + 1
 
-    print("Done drawing path of least resistance on map!")
-    return image
+        return next_pixel_row, next_pixel_column
 
 
 if __name__ == "__main__":
-    # Read the data from elevation_small.txt into an appropriate data structure to get the elevation data.
-    with open('elevation_small.txt') as file:
-        elevation_data = read_elevation_data_into_2d_list(file)
+    # create new ElevationMap object with the elevation data file
+    elevation_map = ElevationMap('elevation_small.txt')
+
+    # create new ElevationMapPainer object with the ElevationMap object
+    elevation_map_painter = ElevationMapPainter(elevation_map)
 
     # Using the Pillow library, create an elevation map from the data. Higher elevations should be brighter; lower elevations darker.
-    image = draw_elevation_map_rgba(elevation_data)
+    image = elevation_map_painter.draw_elevation_map_rgba()
 
-    # given a starting point, loop through all the columns with greedy_walk_to_next_pixel until get to the end
-    pixel_row = 300
+    # given an image and a starting point, loop through all the columns with greedy_walk_to_next_pixel until get to the end
+    # start at the middle pixel on the east side of the map
+    pixel_row = len(elevation_map.elevation_data[0]) // 2
     pixel_column = 0
-    image = draw_path_of_least_resistance(image, elevation_data, pixel_row, pixel_column)
+    image = elevation_map_painter.draw_path_of_least_resistance(image, pixel_row, pixel_column)
 
 
     # TODO:
--- test_pathfinder.py
+++ test_pathfinder.py
@@ -1,29 +1,30 @@
-import pathfinder
+from pathfinder import ElevationMap, ElevationMapPainter, GreedyAlgorithm
+
+elevation_map = ElevationMap('elevation_small.txt')
+elevation_map_painter = ElevationMapPainter(elevation_map)
 
 def test_file_can_be_read_into_a_2d_list():
     # the first value in elevation_small is 4713
     # the last value in elevation_small is 3948
-    file = open('elevation_small.txt')
-    elevation_data = pathfinder.read_elevation_data_into_2d_list(file)
-    assert(elevation_data[0][0] == 4713)
-    assert(elevation_data[-1][-1] == 3948)
+    assert(elevation_map.elevation_data[0][0] == 4713)
+    assert(elevation_map.elevation_data[-1][-1] == 3948)
 
 
 def test_max_elevation_can_be_found():
-    data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    assert(pathfinder.find_max_elevation(data) == 9)
+    # 5648 is the max elevation in elevation_small.txt
+    assert(elevation_map_painter.find_max_elevation() == 5648)
 
 
 def test_min_elevation_can_be_found():
-    data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    assert(pathfinder.find_min_elevation(data) == 1)
+    # 3139 is the max elevation in elevation_small.txt
+    assert(elevation_map_painter.find_min_elevation() == 3139)
 
 
 def test_alpha_value_can_be_calculated():
-    min_value = 1
-    max_value = 9
-    assert(pathfinder.calculate_alpha_value(min_value, max_value, 9) == 255)
-    assert(pathfinder.calculate_alpha_value(min_value, max_value, 1) == 0)
+    # max elevation should be 255
+    assert(elevation_map_painter.calculate_alpha_value(elevation_map_painter.max_elevation) == 255)
+    # min elevation should be 0
+    assert(elevation_map_painter.calculate_alpha_value(elevation_map_painter.min_elevation) == 0)
 
 
 def test_next_pixel_can_be_found_with_greedy_algorithm():
@@ -33,9 +34,10 @@ def test_next_pixel_can_be_found_with_greedy_algorithm():
     # 48 10 112
     # 74 9  28
     two_d_list = [[11, 3, 146], [43, 8, 54], [2, 8, 43], [48, 10, 112], [74, 9, 28]]
+    greedy_algorithm = GreedyAlgorithm(two_d_list)
     start_row = 2
     start_column = 0
-    next_pixel_row, next_pixel_column = pathfinder.greedy_walk_to_next_pixel(two_d_list, start_row, start_column)
+    next_pixel_row, next_pixel_column = greedy_algorithm.greedy_walk_to_next_pixel(start_row, start_column)
 
     assert(next_pixel_row == 2)
     assert(next_pixel_column == start_column + 1)
